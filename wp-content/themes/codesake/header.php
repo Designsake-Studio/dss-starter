01ade0f4c147722b966154b1cb3877d78c450d50
Galen Strasen <galen@wearetmbr.com>
2020-03-17 14:47:37 -0700

    Remove conditional html5 shim

--- wp-content/themes/codesake/header.php
+++ wp-content/themes/codesake/header.php
@@ -15,11 +15,6 @@
 
 	<title><?php wp_title(' | ', true, 'right'); ?></title>
 
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]>
-	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
 
   <link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_directory');?>/public/images/favicon/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_directory');?>/public/images/favicon/favicon-32x32.png">
