e6cd76727160586ed8d38a9904ee5d4cd7662079
Galen Strasen <galen@wearetmbr.com>
2020-03-17 16:49:26 -0700

    JS clean up

--- wp-content/themes/codesake/assets/scripts/Control.js
+++ wp-content/themes/codesake/assets/scripts/Control.js
@@ -1,61 +1,33 @@
-/**
- * This function is called at Document.ready()
- * @link https://github.com/documentationjs/documentation/blob/master/docs/GETTING_STARTED.md
- * @name Control
- * @private
- * @param {object} jQuery
- * @returns {object} Control + methods
- * @example Control.init();
-*/
+/*!
+ * All sorts javascript/jQuery functions
+ *
+ * @author      Designsake Studio
+ * @version     1.0.0
+ *
+ */
 
 
-var Control = function($) {
+/*==================================================================================
+  Functions
+==================================================================================*/
 
-  var _init = function() {
+(function ($, root, undefined) {
+  $(function () {
+    'use strict';
 
-    // Animated.init();
-    // DetectPortrait.init();
-    // Lightbox.init();
-    // MobileDetect.init();
-    Preloader.init();
-    // StickyNav.init();
-    // SlickSlider.init();
-    // Throttle.init();
-    // SmoothScroll.init();
-
-  };
-
-  return {
-      init: _init
-  };
-
-}(jQuery);
-
-
-
-/**
- * This function fires off Control.init();
-*/
-jQuery(document).ready(function() {
-
-    Control.init();
-
-    /**
-     * Grab Instagram feed
-     * @link http://instafeedjs.com/
-    */
-    if($('#instafeed').length) {
-    var userFeed = new Instafeed({
-      get: 'user',
-      userId: '34970505',
-      target: 'insta-target',
-      limit: 8,
-      accessToken: '34970505.1677ed0.6ed1f38f15cb4c09896281ba992040a5',
-      resolution: 'low_resolution',
-      template: '<a href="{{link}}" target="_blank"><div class="ig-background" style="background-image: url({{image}});"><div class="ig-caption"><span class="likes"><i class="fa fa-heart fa-fw"></i> {{likes}}</span></div></div></a>'
+    /* Preloader
+    /––––––––––––––––––––––––*/
+    $(window).on('load', function(){
+      setTimeout(function() {
+        $('.js-sitewrap').animate({
+          opacity: 1
+        }, 300);
+        $('.js-preloader').fadeOut(300, function() {
+          Animated.init();
+        });
+      }, 300);
     });
 
-    userFeed.run();
+  });
 
-  }
-});
+})(jQuery, this);
--- wp-content/themes/codesake/assets/scripts/modules/Animated.js
+++ wp-content/themes/codesake/assets/scripts/modules/Animated.js
@@ -1,29 +1,10 @@
-/**
- * @name Animated
- * @private
- * @param {object} jQuery
- * @returns {object} Animated.init
- * * @returns {object} Animated.ShowAnimated
-*/
+var Animated = function($) {
 
-
-var Animated = function($) { // ----- static module
-  // private var(s)
-
-
-  // private method(s)
   var _init = function() {
-
-    // TODO: Make this fuction work with passed in selector
-
     $('.animation, .animation-visible').each(function() {
-
       var $element = $(this);
-
       $element.waypoint(function() {
-
         var delay = 0;
-
         if ($element.attr('data-delay')) delay = parseInt($element.attr('data-delay'), 0);
         if (!$element.hasClass('animated')) {
           setTimeout(function() {
@@ -34,22 +15,15 @@ var Animated = function($) { // ----- static module
       }, {
         offset: '80%'
       });
-
     });
-
-    console.log("animated init");
   };
 
-  // Show All Animated Items
   var _showAnimated = function() {
-
     $('.animation, .animation-visible').each(function() {
       $(this).addClass('animated');
     });
-
   };
 
-  // output/public
   return {
     init: _init,
     ShowAnimated: _showAnimated
