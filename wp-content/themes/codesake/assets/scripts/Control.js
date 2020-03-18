ccd7abb4c61e9a97f45e8a84fb3f6f3bb8df5186
Galen Strasen <galen@wearetmbr.com>
2020-03-17 17:45:09 -0700

    JS clean up

--- wp-content/themes/codesake/assets/scripts/Control.js
+++ wp-content/themes/codesake/assets/scripts/Control.js
@@ -28,6 +28,42 @@
       }, 300);
     });
 
+    /* Smooth Scroll
+    /––––––––––––––––––––––––*/
+
+    // to anchor
+    $('a[href*="#"]:not([href="#"]).scroll-to').on('click','', function(e) {
+      e.preventDefault();
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate(
+            { scrollTop: target.offset().top },
+            { duration: 600, easing:'easeOutCubic'}
+          );
+          return false;
+        }
+      }
+    });
+
+    // scrolls to anchor on new page if there is hash in URL
+    if(window.location.hash) {
+      var hash = window.location.hash.substring(1), //Puts hash in variable, and removes the # character
+          target = $('.hash-'+ hash);
+
+      $(window).on("load", function(){
+        if (target.length) {
+          $('html,body').animate(
+            { scrollTop: target.offset().top },
+            { duration: 1000, easing:'easeOutCubic'}
+          ).delay(300);
+          return false;
+        }
+      });
+    }
+
+
   });
 
 })(jQuery, this);
--- wp-content/themes/codesake/assets/scripts/modules/Throttle.js
+++ wp-content/themes/codesake/assets/scripts/modules/Throttle.js
@@ -1,14 +1,15 @@
-// Credit goes to [Underscore.js](http://underscorejs.org/)
+/* THROTTLE
+/––––––––––––––––––––––––*/
+// http://underscorejs.org/
 
-/**
- * Returns a function, that, when invoked, will only be triggered at most
+/* Returns a function, that, when invoked, will only be triggered at most
  * once during a given window of time. Normally, the throttled function will
  * run as much as it can, without ever going more than once per wait
  * duration; but if you’d like to disable the execution on the leading edge,
  * pass {leading: false}. To disable execution on the trailing edge, ditto.
  */
 
-// throttle's dependent upon _now
+// throttle is dependent upon _now
 _now = Date.now || function() {
   return new Date().getTime();
 };
